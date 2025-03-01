915d5bc06b80e090f810198de0606754f3bbec0c
Roshan <roshanochu09@gmail.com>
2025-03-01 13:08:22 +0530

    created basic folders and files

---
+++ lib/view/fast_laughs/fastlaughs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FastLaughScreen extends StatelessWidget {
+  const FastLaughScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text("fast laughs"),
+      )),
+    );
+  }
+}
---
+++ lib/view/new&hot/new_hot_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewHotScreen extends StatelessWidget {
+  const NewHotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text("new and hot"),
+      )),
+    );
+  }
+}
