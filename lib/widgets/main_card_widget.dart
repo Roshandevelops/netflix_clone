f49c64fc913fb5594aa9601b00318038e7317783
Roshan <roshanochu09@gmail.com>
2025-03-19 16:11:34 +0530

    completed all the apois in home and search,no issues left

--- lib/widgets/main_card_widget.dart
+++ lib/widgets/main_card_widget.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_project/core/strings.dart';
 import 'package:netflix_project/models/movie_model.dart';
+import 'package:netflix_project/models/new_hot_model.dart';
 import 'package:netflix_project/widgets/constants.dart';
 
 class MainCardWidget extends StatelessWidget {
-  const MainCardWidget(
-      {super.key,
-      required this.width,
-      this.height,
-      required this.searchModel,
-      this.imageUrl});
+  const MainCardWidget({
+    super.key,
+    required this.width,
+    this.height,
+    this.searchModel,
+    this.imageUrl,
+    this.newAndHotModel,
+  });
   final double? width;
   final double? height;
   final MovieModel? searchModel;
+  final NewAndHotModel? newAndHotModel;
 
   final String? imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    String imageUrl = searchModel?.posterPath != null
-        ? "$imageAppendUrl${searchModel!.posterPath}"
-        : "https://media.themoviedb.org/t/p/w500_and_h282_face/kYgQzzjNis5jJalYtIHgrom0gOx.jpg";
+    String finalImageUrl = imageUrl ??
+        (searchModel?.posterPath != null
+            ? "$imageAppendUrl${searchModel!.posterPath}"
+            : newAndHotModel?.posterPath != null
+                ? "$imageAppendUrl${newAndHotModel!.posterPath}"
+                : "https://media.themoviedb.org/t/p/w500_and_h282_face/kYgQzzjNis5jJalYtIHgrom0gOx.jpg");
+    if (finalImageUrl.isEmpty) {
+      finalImageUrl =
+          "https://media.themoviedb.org/t/p/w500_and_h282_face/kYgQzzjNis5jJalYtIHgrom0gOx.jpg";
+    }
     return Row(
       children: [
         Container(
@@ -30,7 +41,7 @@ class MainCardWidget extends StatelessWidget {
             color: const Color(0xFF4F4C4C),
             image: DecorationImage(
               fit: BoxFit.cover,
-              image: NetworkImage(imageUrl),
+              image: NetworkImage(finalImageUrl),
             ),
             borderRadius: kBorderradius,
           ),
--- lib/widgets/title_image_widget.dart
+++ lib/widgets/title_image_widget.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_project/controller/home_provider.dart';
+import 'package:netflix_project/models/new_hot_model.dart';
 import 'package:netflix_project/widgets/constants.dart';
 import 'package:netflix_project/widgets/main_card_widget.dart';
 import 'package:netflix_project/widgets/title_widget.dart';
+import 'package:provider/provider.dart';
 
 class TitleImageWidget extends StatelessWidget {
-  const TitleImageWidget(
-      {super.key, required this.title, required this.posterList});
+  const TitleImageWidget({
+    super.key,
+    required this.title,
+    required this.posterList,
+    this.newAndHotModel,
+  });
 
   final String title;
   final List<String> posterList;
+  final NewAndHotModel? newAndHotModel;
 
   @override
   Widget build(BuildContext context) {
@@ -20,24 +28,27 @@ class TitleImageWidget extends StatelessWidget {
         ),
         LimitedBox(
           maxHeight: 180,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: List.generate(
-              posterList.length,
-              (index) {
-                return Row(
-                  children: [
-                    MainCardWidget(
-                      width: 120,
-                      height: 200,
-                      searchModel: null,
-                    ),
-                    kWidth,
-                  ],
-                );
-              },
-            ),
-          ),
+          child: Consumer<HomeProvider>(builder: (context, value, child) {
+            return ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                posterList.length,
+                (index) {
+                  return Row(
+                    children: [
+                      MainCardWidget(
+                        newAndHotModel: value.pastYearList[index],
+                        width: 120,
+                        height: 200,
+                        // searchModel: null,
+                      ),
+                      kWidth,
+                    ],
+                  );
+                },
+              ),
+            );
+          }),
         ),
       ],
     );
